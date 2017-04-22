dd99b4d81c3849df74a663efad9a1322a09151f5
Vinicius Brasil <marcosbrasilaraujo@gmail.com>
2017-04-22 12:11:48 -0300

    Create problem-7.py

---
+++ problem-7.py
@@ -0,0 +1,22 @@
+def is_prime(num):
+    prime_counter = 1
+    for x in range(1, num):
+        if num % x == 0:
+            prime_counter += 1
+        if prime_counter > 2:
+            return False
+    return True
+
+primes = []
+i = 1
+p = 0
+n = 10001
+
+
+while p != n+1:
+    if is_prime(i):
+        primes.append(i)
+        p += 1        
+    i += 1
+        
+print(primes[n])
